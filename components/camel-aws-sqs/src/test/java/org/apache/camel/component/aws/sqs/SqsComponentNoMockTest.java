c6342bef9c2384d94ed085fec670e3660b8cf0ec
Otavio R. Piske <angusyoung@gmail.com>
2019-09-28 10:58:51 +0200

    WIP: integrated test for the AWS-SQS component

---
+++ components/camel-aws-sqs/src/test/java/org/apache/camel/component/aws/sqs/SqsComponentNoMockTest.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.sqs;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.apache.camel.*;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SqsComponentNoMockTest extends CamelTestSupport {
+
+    @EndpointInject("direct:start")
+    private ProducerTemplate template;
+
+    @EndpointInject("mock:result")
+    private MockEndpoint result;
+
+    @BindToRegistry("amazonSQSClient")
+    private AmazonSQS client;
+
+    @Before
+    public void setup() {
+        client = AmazonSQSClientBuilder.standard().withCredentials(new AWSCredentialsProvider() {
+            @Override
+            public AWSCredentials getCredentials() {
+                return new AWSCredentials() {
+                    @Override
+                    public String getAWSAccessKeyId() {
+                        return System.getProperty("aws.access.key");
+                    }
+
+                    @Override
+                    public String getAWSSecretKey() {
+                        return System.getProperty("aws.secret.key");
+                    }
+                };
+            }
+
+            @Override
+            public void refresh() {
+
+            }
+        }).build();
+    }
+
+    @Test
+    public void sendInOnly() throws Exception {
+        result.expectedMessageCount(1);
+
+        Exchange exchange = template.send("direct:start", ExchangePattern.InOnly, new Processor() {
+            public void process(Exchange exchange) throws Exception {
+                exchange.getIn().setBody("This is my message text.");
+            }
+        });
+
+        assertMockEndpointsSatisfied();
+
+        Exchange resultExchange = result.getExchanges().get(0);
+        assertEquals("This is my message text.", resultExchange.getIn().getBody());
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.MESSAGE_ID));
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.RECEIPT_HANDLE));
+        assertEquals("6a1559560f67c5e7a7d5d838bf0272ee", resultExchange.getIn().getHeader(SqsConstants.MD5_OF_BODY));
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.ATTRIBUTES));
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.MESSAGE_ATTRIBUTES));
+
+        assertEquals("This is my message text.", exchange.getIn().getBody());
+        assertNotNull(exchange.getIn().getHeader(SqsConstants.MESSAGE_ID));
+        assertEquals("6a1559560f67c5e7a7d5d838bf0272ee", exchange.getIn().getHeader(SqsConstants.MD5_OF_BODY));
+    }
+
+    @Test
+    public void sendInOut() throws Exception {
+        result.expectedMessageCount(1);
+
+        Exchange exchange = template.send("direct:start", ExchangePattern.InOut, new Processor() {
+            public void process(Exchange exchange) throws Exception {
+                exchange.getIn().setBody("This is my message text.");
+            }
+        });
+
+        assertMockEndpointsSatisfied();
+
+        Exchange resultExchange = result.getExchanges().get(0);
+        assertEquals("This is my message text.", resultExchange.getIn().getBody());
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.RECEIPT_HANDLE));
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.MESSAGE_ID));
+        assertEquals("6a1559560f67c5e7a7d5d838bf0272ee", resultExchange.getIn().getHeader(SqsConstants.MD5_OF_BODY));
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.ATTRIBUTES));
+        assertNotNull(resultExchange.getIn().getHeader(SqsConstants.MESSAGE_ATTRIBUTES));
+
+        assertEquals("This is my message text.", exchange.getOut().getBody());
+        assertNotNull(exchange.getOut().getHeader(SqsConstants.MESSAGE_ID));
+        assertEquals("6a1559560f67c5e7a7d5d838bf0272ee", exchange.getOut().getHeader(SqsConstants.MD5_OF_BODY));
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        final String queue = System.getProperty("aws.sqs.queue");
+        final String accessKey = System.getProperty("aws.access.key");
+        final String secretKey = System.getProperty("aws.secret.key");
+
+        return new RouteBuilder() {
+            final String sqsURI = String.format("aws-sqs://%s?accessKey=%s&secretKey=%s&region=US_WEST_2&amazonAWSHost=amazonaws.com&messageRetentionPeriod=%s&maximumMessageSize=%s",
+                    queue, accessKey, secretKey, "1209600", "65536");
+
+
+            @Override
+            public void configure() throws Exception {
+                System.out.println("Using queue: " + sqsURI);
+
+                from("direct:start").to(sqsURI);
+
+                from(sqsURI).to("mock:result");
+            }
+        };
+    }
+}

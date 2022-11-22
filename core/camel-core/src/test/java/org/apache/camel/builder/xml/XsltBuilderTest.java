2bc6e6b4ea34a095d211d56f4d3650adb1f2e916
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-11-22 18:40:51 +0100

    WIP SSC-3: avoid cast in the core code

--- core/camel-core/src/test/java/org/apache/camel/builder/xml/XsltBuilderTest.java
+++ core/camel-core/src/test/java/org/apache/camel/builder/xml/XsltBuilderTest.java
@@ -26,14 +26,9 @@ import javax.xml.transform.Source;
 import javax.xml.transform.Templates;
 import javax.xml.transform.sax.SAXSource;
 
-import org.w3c.dom.Document;
-
-import org.xml.sax.InputSource;
-
 import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExpectedBodyTypeException;
-import org.apache.camel.ExtendedExchange;
 import org.apache.camel.component.xslt.StreamResultHandlerFactory;
 import org.apache.camel.component.xslt.XsltBuilder;
 import org.apache.camel.converter.jaxp.XmlConverter;
@@ -42,6 +37,8 @@ import org.apache.camel.support.DefaultExchange;
 import org.apache.camel.support.UnitOfWorkHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -261,7 +258,7 @@ public class XsltBuilderTest extends ContextTestSupport {
         assertTrue(body.endsWith("<goodbye>world!</goodbye>"));
 
         // now done the exchange
-        List<Synchronization> onCompletions = exchange.adapt(ExtendedExchange.class).handoverCompletions();
+        List<Synchronization> onCompletions = exchange.getExchangeExtension().handoverCompletions();
         UnitOfWorkHelper.doneSynchronizations(exchange, onCompletions, log);
 
         // the file should be deleted

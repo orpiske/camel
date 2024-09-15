4f5fd09de0f1eb9714fffa023f467ef9f8098424
Otavio R. Piske <angusyoung@gmail.com>
2024-09-15 10:19:06 +0200

    WIP CAMEL-21080: create a new interface for the legacy methods

---
+++ components/camel-test/camel-test-junit5/src/main/java/org/apache/camel/test/junit5/legacy/LegacyTestSupport.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.test.junit5.legacy;
+
+import org.apache.camel.CamelContext;
+
+/**
+ * An interface containing all the legacy methods for code willing to rely on their own implementation of the
+ * CamelTestSupport as existed on Camel 4.8 and older.
+ */
+public interface LegacyTestSupport {
+    /**
+     * Use the RouteBuilder or not
+     *
+     * @return <tt>true</tt> then {@link CamelContext} will be auto started, <tt>false</tt> then {@link CamelContext}
+     *         will <b>not</b> be auto started (you will have to start it manually)
+     */
+    boolean isUseRouteBuilder();
+}
